36f1c40db8a07e11235e827f83e9c3aff35e9d9e
Shawn-Benedict <shawnbenedict273@gmail.com>
2021-02-04 23:34:08 -0500

    Score function returns percent as float

--- ML Dataset Generation/utils.py
+++ ML Dataset Generation/utils.py
@@ -48,11 +48,35 @@ def load_data(filename):
 
 
 def score(y_true, y_pred):
+    # Find how many nodes the path takes
     unique, counts = np.unique(y_true, return_counts=True)
     count_dict = dict(zip(unique, counts))
     len_path = count_dict[1]
 
-    ind = np.argpartition(y_pred, -len_path)[-len_path:]
+    # turn y_pred into dict sorted by key (predicted value). Has values as index before being sorted
+
+    y_dict = {}
+    for i in range(y_pred):
+        y_dict[y_pred[i]] = i
+
+    y_dict.sort()  # might be wrong syntax but I'm pretty sure this works
+    amount_before_done = []
+    nodes_checked = 0
+
+    for i in y_dict:
+        nodes_checked += 1
+        if y_true(i) == 1:
+            amount_before_done += 1
+        if len(amount_before_done) >= len_path:
+            break
+
+    # percent as decimal of score where 100 is amount_checked == y_path and slowly deteriorates after that
+
+    l = np.linspace(0, 1, num=(2601 - len_path))[::-1]
+
+    return l[amount_before_done - len_path]
+
+    # ind = np.argpartition(y_pred, -len_path)[-len_path:]
 
 
 def ReLu(x):
@@ -63,11 +87,11 @@ def ReLu(x):
 
 
 def sigmoid(x):
-    return 1/(1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 def cross_entropy(y_pred, y_true):
-    return -np.sum((y_true * math.log10(y_pred))+(1 - y_true) * math.log10(1 - y_pred))
+    return -np.sum((y_true * math.log10(y_pred)) + (1 - y_true) * math.log10(1 - y_pred))
 
 
 if __name__ == "__main__":
